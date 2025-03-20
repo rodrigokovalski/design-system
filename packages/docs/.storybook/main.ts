d838e91cd997d45739bf00a1fc242687826e1745
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-20 12:27:27 -0300

    fix: changing permissions to deploy

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,12 @@ const config: StorybookConfig = {
     "name": getAbsolutePath('@storybook/react-vite'),
     "options": {}
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
